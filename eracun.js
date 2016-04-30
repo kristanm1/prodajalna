75cd01d637717401bbc1bf0ec2c9d10d81511b5b
kristanm1 <kristanm1@gmail.com>
2016-04-30 14:46:10 +0000

    Zaključek dela na IV. nalogi

--- eracun.js
+++ eracun.js
@@ -160,14 +160,27 @@ streznik.get('/izpisiRacun/:oblika', function(zahteva, odgovor) {
       odgovor.send("<p>V košarici nimate nobene pesmi, \
         zato računa ni mogoče pripraviti!</p>");
     } else {
-      odgovor.setHeader('content-type', 'text/xml');
-      odgovor.render('eslog', {
-        vizualiziraj: zahteva.params.oblika == 'html' ? true : false,
-        postavkeRacuna: pesmi
-      })  
+      dobiStranko(zahteva.session.stranka, function(napaka, stranka) {
+        if(napaka) {odgovor.sendStatus(500);}
+        else {
+          odgovor.setHeader('content-type', 'text/xml');
+          odgovor.render('eslog', {
+            vizualiziraj: zahteva.params.oblika == 'html' ? true : false,
+            postavkeRacuna: pesmi,
+            stranka: stranka[0]
+          });
+        }
+      });
     }
   })
 })
+
+var dobiStranko = function(strankaID, callback) {
+  pb.all("SELECT * FROM Customer WHERE Customer.CustomerId = " + strankaID, 
+  function(napaka, vrstice) {
+      callback(napaka, vrstice);
+  })
+}
 
 // Privzeto izpiši račun v HTML obliki
 streznik.get('/izpisiRacun', function(zahteva, odgovor) {
@@ -233,6 +246,7 @@ streznik.post('/stranka', function(zahteva, odgovor) {
   var form = new formidable.IncomingForm();
   
   form.parse(zahteva, function (napaka1, polja, datoteke) {
+    zahteva.session.stranka = polja.seznamStrank;
     odgovor.redirect('/')
   });
 })
